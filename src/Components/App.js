ab1872cdbb9311ddac9110147a39a621785f7184
DaJuan Harris <dajuancancode@gmail.com>
2020-08-11 17:04:55 -0400

    Finishing touches on website

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import Header from "./Header/Header"
-import Services from "./Services/Services"
-import About from "./About/About"
-import Work from "./Work/Work"
-import Contact from "./Contact/Contact"
-import Footer from "./Footer/Footer"
+import Header from "./Header/Header";
+import Services from "./Services/Services";
+import About from "./About/About";
+import Work from "./Work/Work";
+import Contact from "./Contact/Contact";
+import Footer from "./Footer/Footer";
 
-import styles from "./App.module.sass"
-
+import styles from "./App.module.sass";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Header  id="home"/>
-      <Services id="services"/>
-      <About id="about"/>
-      <Work id="work"/>
-      <Contact id="contact"/>
-      <Footer/>
+      <Header id="home" />
+      <Services id="services" />
+      <About id="about" />
+      <Work id="events" />
+      <Contact id="contact" />
+      <Footer />
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,49 +1,87 @@
-import React, {useState} from 'react';
-import Burger from '@animated-burgers/burger-squeeze'
-import '@animated-burgers/burger-squeeze/dist/styles.css'
-import styles from './Navbar.module.sass';
+import React, { useState } from "react";
+import Burger from "@animated-burgers/burger-squeeze";
+import "@animated-burgers/burger-squeeze/dist/styles.css";
+import styles from "./Navbar.module.sass";
 
-const Navabar = props => {
-
-  const logo = "https://res.cloudinary.com/dajuancancode/image/upload/v1593460166/you-need-a-mc/logo.svg";
+const Navabar = (props) => {
+  const logo =
+    "https://res.cloudinary.com/dajuancancode/image/upload/v1593460166/you-need-a-mc/logo.svg";
 
   const [status, setStatus] = useState(false);
 
   const handleClick = () => {
-    const navigation = document.querySelector("#navigation")
-    navigation.style.transform = !status ? "scale(1)" : "scale(0)"
-    setStatus(!status)
-  }
+    const navigation = document.querySelector("#navigation");
+    navigation.style.transform = !status ? "scale(1)" : "scale(0)";
+    setStatus(!status);
+  };
 
   return (
     <nav className={styles.Navbar}>
       <div className={styles.Navbar__logoContainer}>
-        <img src={logo} className={styles.Navbar__logo} alt="You Need A MC logo"/>
+        <img
+          src={logo}
+          className={styles.Navbar__logo}
+          alt="You Need A MC logo"
+        />
       </div>
       <button className={styles.Navbar__menuButton}>
-        <Burger className={styles.Navbar__menuIcon} isOpen={status} onClick={handleClick}/>
+        <Burger
+          className={styles.Navbar__menuIcon}
+          isOpen={status}
+          onClick={handleClick}
+        />
       </button>
       <div className={styles.Navbar__navigation} id="navigation">
         <ul className={styles.Navbar__nav}>
-        <li className={styles.Navbar__navItem}>
-            <a className={styles.Navbar__navLink} onClick={handleClick} href="#home">Home</a>
+          <li className={styles.Navbar__navItem}>
+            <a
+              className={styles.Navbar__navLink}
+              onClick={handleClick}
+              href="#home"
+            >
+              Home
+            </a>
           </li>
           <li className={styles.Navbar__navItem}>
-            <a className={styles.Navbar__navLink} onClick={handleClick} href="#services">Services</a>
+            <a
+              className={styles.Navbar__navLink}
+              onClick={handleClick}
+              href="#services"
+            >
+              Services
+            </a>
           </li>
           <li className={styles.Navbar__navItem}>
-            <a className={styles.Navbar__navLink} onClick={handleClick} href="#about">About</a>
+            <a
+              className={styles.Navbar__navLink}
+              onClick={handleClick}
+              href="#about"
+            >
+              About
+            </a>
           </li>
           <li className={styles.Navbar__navItem}>
-            <a className={styles.Navbar__navLink} onClick={handleClick} href="#work">Work</a>
+            <a
+              className={styles.Navbar__navLink}
+              onClick={handleClick}
+              href="#events"
+            >
+              Past Events
+            </a>
           </li>
           <li className={styles.Navbar__navItem}>
-            <a className={styles.Navbar__navLink} onClick={handleClick} href="#contact">Contact</a>
+            <a
+              className={styles.Navbar__navLink}
+              onClick={handleClick}
+              href="#contact"
+            >
+              Contact
+            </a>
           </li>
         </ul>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navabar
+export default Navabar;
